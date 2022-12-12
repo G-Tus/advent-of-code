5a6534a4512e2ed3c09ae65d47ce4bad7978199e
Gert-Jan van der Ham <github@g-tus.nl>
2022-12-12 18:04:45 +0100

    Refactoring for part 2, completed 2022 day 9 part 2

--- 2022/day9/python/2022_day9.py
+++ 2022/day9/python/2022_day9.py
@@ -5,10 +5,13 @@ def ropebridge():
     with open("../input.txt", "r") as file:
         steps = file.read().splitlines()
 
-    head = [0, 0]
-    tail = [0, 0]
-    count = 1
-    history = []
+    length = 10
+    knots = []
+    for _ in range(10):
+        knots.append([0, 0])
+
+    count = {1: 1, 9: 1}
+    history = {1: [[0, 0]], 9: [[0, 0]]}
 
     for step in steps:
         move, amount = step.split()
@@ -24,20 +27,27 @@ def ropebridge():
                 transform = [1, 0]
 
         for _ in range(int(amount)):
-            previous = deepcopy(head)
-            head[0] += transform[0]
-            head[1] += transform[1]
-
-            diff_x = head[0] - tail[0]
-            diff_y = head[1] - tail[1]
-
-            if diff_x > 1 or diff_x < -1 or diff_y > 1 or diff_y < -1:
-                tail = deepcopy(previous)
-                if tail not in history:
-                    count += 1
-                    history.append(tail)
+            knots[0][0] += transform[0]
+            knots[0][1] += transform[1]
+            pass
+            for index in range(1, length):
+                
+                previous = index - 1
+                diff = [knots[previous][0] - knots[index][0], knots[previous][1] - knots[index][1]]
+                if -2 not in diff and 2 not in diff:
+                    continue
+                for i, gap in enumerate(diff):
+                    if abs(gap) == 2:
+                        knots[index][i] += int(gap / 2)
+                    elif abs(gap) == 1:
+                        knots[index][i] += gap
+                if index in count:
+                    if knots[index] not in history[index]:
+                        count[index] += 1
+                        history[index].append(deepcopy(knots[index]))
     
-    print(f"Positions visited by tail: {count}")
+    print(f"Positions visited by tail 1: {count[1]}")
+    print(f"Positions visited by tail 9: {count[9]}")
 
 if __name__ == "__main__":
 
