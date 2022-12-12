084a941a3c0c37271b29b4ec985f119a37d36ffd
Gert-Jan van der Ham <github@g-tus.nl>
2022-12-12 18:12:15 +0100

    Minor changes to 2022 day 9 Python script

--- 2022/day9/python/2022_day9.py
+++ 2022/day9/python/2022_day9.py
@@ -7,7 +7,7 @@ def ropebridge():
 
     length = 10
     knots = []
-    for _ in range(10):
+    for _ in range(length):
         knots.append([0, 0])
 
     count = {1: 1, 9: 1}
@@ -29,18 +29,20 @@ def ropebridge():
         for _ in range(int(amount)):
             knots[0][0] += transform[0]
             knots[0][1] += transform[1]
-            pass
+
             for index in range(1, length):
-                
                 previous = index - 1
                 diff = [knots[previous][0] - knots[index][0], knots[previous][1] - knots[index][1]]
+
                 if -2 not in diff and 2 not in diff:
                     continue
+
                 for i, gap in enumerate(diff):
                     if abs(gap) == 2:
-                        knots[index][i] += int(gap / 2)
+                        knots[index][i] += gap // 2
                     elif abs(gap) == 1:
                         knots[index][i] += gap
+
                 if index in count:
                     if knots[index] not in history[index]:
                         count[index] += 1
