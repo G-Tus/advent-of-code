c24907fa4290ddbd4ee79ac01e85394537a52acd
Gert-Jan van der Ham <github@g-tus.nl>
2023-03-09 20:24:54 +0100

    Fixed 2022 day 1 in TypeScript

--- 2022/day01/typescript/2022_day01.ts
+++ 2022/day01/typescript/2022_day01.ts
@@ -1,4 +1,19 @@
 import { readFileSync } from 'fs';
 
-const file = readFileSync('../input.txt')
-console.log(file)
+const lines: string[] = readFileSync('../input.txt', 'utf8').split("\n");
+
+var elf: number = 0;
+var backpacks: number[] = [0];
+
+for (var line of lines) {
+    if (line == "") {
+        elf += 1;
+        backpacks[elf] = 0;
+    }
+    backpacks[elf] += Number(line);
+}
+
+console.log(`Calories of most filled backpack: ${Math.max(...backpacks)}`);
+
+backpacks.sort((n1, n2) => {return n2 - n1;});
+console.log(`Sum of 3 most filled backpacks ${backpacks.slice(0, 3).reduce((sum, a) => sum + a, 0)}`)
