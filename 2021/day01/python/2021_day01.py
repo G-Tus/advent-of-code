84849f06422784f97e64a89fe3617bb4e87afcbc
Gert-Jan van der Ham <github@g-tus.nl>
2023-12-03 16:41:10 +0100

    Fixed 2021 day 1 part 2 in Python

--- 2021/day01/python/2021_day01.py
+++ 2021/day01/python/2021_day01.py
@@ -3,7 +3,8 @@ def depth():
     with open("../input.txt", "r") as file:
         data = file.read().splitlines()
 
-    increased = 0
+    part1 = 0
+    part2 = 0
 
     previous = int(data[0])
 
@@ -11,11 +12,22 @@ def depth():
         current = int(current)
 
         if current > previous:
-            increased += 1
+            part1 += 1
 
         previous = current
 
-    print(f"Times depth increased: {increased}")
+    previous = sum([int(number) for number in data[0:3]])
+
+    for index, current in enumerate(data[1:-2], 1):
+        current = int(current) + int(data[index + 1]) + int(data[index + 2])
+
+        if current > previous:
+            part2 += 1
+
+        previous = current
+
+    print(f"Times depth increased part 1: {part1}")
+    print(f"Times depth increased part 2: {part2}")
 
 if __name__ == "__main__":
     depth()
