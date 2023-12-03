6f0a3cfb6beb09b74375b3c1b0d72bbb951e5484
Gert-Jan van der Ham <github@g-tus.nl>
2023-12-03 16:03:20 +0100

    Fixed 2019 day 1 part 2 in Python

--- 2019/day01/python/2019_day01.py
+++ 2019/day01/python/2019_day01.py
@@ -3,13 +3,25 @@ def fuel():
     with open("../input.txt", "r") as file:
         data = file.read().splitlines()
 
-    total = 0
+    part1 = 0
+    part2 = 0
 
     for module in data:
 
-        total += int(int(module) / 3) - 2
+        mass = int(int(module) / 3) - 2
+        part1 += mass
+        part2 += mass
 
-    print(f"Fuel needed: {total}")
+        while True:
+            mass = int(int(mass) / 3) - 2
+            
+            if mass <= 0:
+                break
+
+            part2 += mass
+
+    print(f"Fuel needed part 1: {part1}")
+    print(f"Fuel needed part 2: {part2}")
 
 if __name__ == "__main__":
     fuel()
