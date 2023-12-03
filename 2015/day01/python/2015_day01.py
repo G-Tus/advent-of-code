2852c92443503622ee7a85614e63b1958b8c3522
Gert-Jan van der Ham <github@g-tus.nl>
2023-12-03 14:27:45 +0100

    Fixed 2015 day 1 part 2 in Python

--- 2015/day01/python/2015_day01.py
+++ 2015/day01/python/2015_day01.py
@@ -4,15 +4,20 @@ def elevator():
         data = file.read()
 
     floor = 0
+    first = None
 
-    for move in data:
+    for position, move in enumerate(data, 1):
         if move == "(":
             floor += 1
 
         elif move == ")":
             floor -= 1
 
+        if not first and floor == -1:
+            first = position
+
     print(f"Santa is on floor: {floor}")
+    print(f"Santa first entered basement on postion: {first}")
 
 if __name__ == "__main__":
     elevator()
