cc134f08398a85c0231af247bd3489a54cffdcb4
Gert-Jan van der Ham <github@g-tus.nl>
2023-12-03 16:20:31 +0100

    Fixed 2020 day 1 part 2 in Python

--- 2020/day01/python/2020_day01.py
+++ 2020/day01/python/2020_day01.py
@@ -3,17 +3,27 @@ def expenses():
     with open("../input.txt", "r") as file:
         data = file.read().splitlines()
 
-    result = None
+    part1 = None
+    part2 = None
+
     for number1 in data:
         for number2 in data:
             if (int(number1) + int(number2)) == 2020:
-                result = int(number1) * int(number2)
+                part1 = int(number1) * int(number2)
+            
+            for number3 in data:
+                if (int(number1) + int(number2) + int(number3)) == 2020:
+                    part2 = int(number1) * int(number2) * int(number3)
+                    break
+
+            if part1 and part2:
                 break
 
-        if result:
+        if part1 and part2:
             break
 
-    print(f"Result: {result}")
+    print(f"Result part 1: {part1}")
+    print(f"Result part 2: {part2}")
 
 if __name__ == "__main__":
     expenses()
