7dd4a065bd4b26a62f1bbaeef34b13850f3abfef
Gert-Jan van der Ham <github@g-tus.nl>
2023-12-03 15:42:49 +0100

    Fixed 2018 day 1 part 2 in Python

--- 2018/day01/python/2018_day01.py
+++ 2018/day01/python/2018_day01.py
@@ -3,12 +3,29 @@ def calibration():
     with open("../input.txt", "r") as file:
           data = file.read().splitlines()
 
-    frequency = 0
+    frequency1 = 0
+    frequency2 = 0
+    frequencies = {0}
+    first = None
 
     for change in data:
-        frequency += int(change)
+        frequency1 += int(change)
 
-    print(f"Resulting frequency: {frequency}")
+    while True:
+        for change in data:
+            frequency2 += int(change)
+            if not first:
+                if frequency2 in frequencies:
+                    first = frequency2
+                    break
+
+            frequencies.add(frequency2)
+
+        if first:
+            break
+
+    print(f"Resulting frequency: {frequency1}")
+    print(f"Frequency that appears twice first: {first}")
 
 if __name__ == "__main__":
     calibration()
