9b84b8d6708048c8b3e4723f7528c68f80b678b1
Gert-Jan van der Ham <github@g-tus.nl>
2023-12-03 13:14:23 +0100

    Fixed 2023 day 3 part 2 in Python

--- 2023/day03/python/2023_day03.py
+++ 2023/day03/python/2023_day03.py
@@ -8,7 +8,10 @@ def engine():
     rows = len(data) - 1
     columns = len(data[0]) - 1
 
-    total = 0
+    part1 = 0
+    part2 = 0
+
+    gears = {}
 
     for i, row in enumerate(data):
         numbers = re.finditer("\d+", row)
@@ -19,14 +22,29 @@ def engine():
             amount = int(number.group())
 
             adjacent = ""
+            
             for y in range(max(i - 1, 0), min(i + 1, rows) + 1):
                 for x in range(max(start - 1, 0), min(stop, columns) + 1):
                     adjacent += data[y][x]
 
-            if re.search(r"[^0-9\.]", adjacent):
-                total += amount
+                    if data[y][x] == "*":
+                        coordinates = f"{x}, {y}"
 
-    print(f"Engine parts summed: {total}")
+                        if coordinates not in gears:
+                            gears[coordinates] = [amount]
+                        else:
+                            gears[coordinates].append(amount)
+
+            if re.search(r"[^0-9\.]", adjacent):
+                part1 += amount
+
+    for gear in gears.values():
+
+        if len(gear) == 2:
+            part2 += gear[0] * gear[1]
+
+    print(f"Engine parts summed: {part1}")
+    print(f"Sum of gear ratios: {part2}")
 
 if __name__ == "__main__":
     engine()
