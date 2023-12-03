024dae860ce9918b2c113f9df734e80652e5a540
Gert-Jan van der Ham <github@g-tus.nl>
2023-12-03 15:20:50 +0100

    Fixed 2017 day 1 part 2 in Python

--- 2017/day01/python/2017_day01.py
+++ 2017/day01/python/2017_day01.py
@@ -3,15 +3,23 @@ def captcha():
     with open("../input.txt", "r") as file:
         data = file.read()
 
-    total = 0
+    length = len(data)
+    halflength = int(length / 2)
+    part1 = 0
+    part2 = 0
 
     for i, current in enumerate(data):
-        next = data[0 if i == len(data) - 1 else i + 1]
+        next1 = data[0 if i == length - 1 else i + 1]
+        next2 = data[i - halflength if i >= halflength else i + halflength]
 
-        if current == next:
-            total += int(current)
+        if current == next1:
+            part1 += int(current)
+
+        if current == next2:
+            part2 += int(current)
         
-    print(f"Solution to captcha: {total}")
+    print(f"Solution to captcha part 1: {part1}")
+    print(f"Solution to captcha part 2: {part2}")
 
 if __name__ == "__main__":
     captcha()
