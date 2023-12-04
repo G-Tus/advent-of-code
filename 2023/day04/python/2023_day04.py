14267bb3a51e94530d2fcb8115cc5bce6ba2418f
Gert-Jan van der Ham <github@g-tus.nl>
2023-12-04 06:37:28 +0100

    Fixed 2023 day 4 in Python

---
+++ 2023/day04/python/2023_day04.py
@@ -0,0 +1,42 @@
+import re
+
+def scratchcards():
+
+    with open("../input.txt", "r") as file:
+        data = file.read().splitlines()
+
+    total = 0
+    counts = {card: 1 for card in range(1, len(data) + 1)}
+
+    for line in data:
+
+        card, numbers = line.split(":")
+
+        card = int(re.search("\d+", card).group())
+
+        winning, have = numbers.split("|")
+
+        winning = re.findall("\d+", winning)
+        have = re.findall("\d+", have)
+
+        value = 0
+        count = 0
+        for number in have:
+
+            if number in winning:
+                count += 1
+                if value == 0:
+                    value = 1
+                else:
+                    value *= 2
+
+        for i in range(card + 1, card + count + 1):
+            counts[i] += counts[card]
+
+        total += value
+
+    print(f"Total points: {total}")
+    print(f"Total scratchcards: {sum(counts.values())}")
+
+if __name__ == "__main__":
+    scratchcards()
