f41fabace2df55507498009f45e650fb01d6dc1c
Gert-Jan van der Ham <github@g-tus.nl>
2023-12-13 12:15:40 +0100

    Fixed day 5 part 2 in Python (brute force) Tested as working, but didn't try actual input (took too long)

--- 2023/day05/python/2023_day05.py
+++ 2023/day05/python/2023_day05.py
@@ -1,53 +1,76 @@
 import re
+import time
 
-def planting():
+class Planting:
+    def __init__(self):
+        self.mapping = {}
+        self.planting()
 
-    with open("../input.txt", "r") as file:
-        data = file.read().splitlines()
+    def seed_location(self, seeds):
+        lowest = 0
 
-    seeds = [int(seed) for seed in re.findall(r"\d+", data[0])]
+        for seed in seeds:
+            source = "seed"
 
-    mapping = {}
+            while True:
+                target = list(self.mapping[source].keys())[0]
 
-    for line in data[1:]:
+                for output, start, width in self.mapping[source][target]:
 
-        if line == "":
-            continue
+                    if (seed >= start) and (seed < (start + width)):
+                        offset = seed - start
+                        seed = output + offset    
+                        break
 
-        elif line[0].isalpha():
-            source_target, _ = line.split(" ")
-            source, _, target = source_target.split("-")
-            mapping[source] = {
-                target: []
-            }
+                if target not in self.mapping:
+                    if (not lowest) or (seed < lowest):
+                        lowest = seed
 
-        elif line[0].isdigit():
-            output, start, width = [int(number) for number in line.split(" ")]
-            mapping[source][target].append([output, start, width])
+                    break          
 
-    outputs = []
+                source = target
 
-    for seed in seeds:
-        source = "seed"
+        return lowest
 
-        while True:
-            target = list(mapping[source].keys())[0]
+    def planting(self):
 
-            for output, start, width in mapping[source][target]:
+        begin = time.perf_counter()
 
-                if (seed >= start) and (seed < (start + width)):
-                    offset = seed - start
-                    seed = output + offset    
-                    break
+        with open("../input.txt", "r") as file:
+            data = file.read().splitlines()
 
-            if target not in mapping:
-                break          
+        seeds = [int(seed) for seed in re.findall(r"\d+", data[0])]
 
-            source = target
+        for line in data[1:]:
 
-        outputs.append(seed)
+            if line == "":
+                continue
 
-    print(f"Lowest location: {min(outputs)}")
+            elif line[0].isalpha():
+                source_target, _ = line.split(" ")
+                source, _, target = source_target.split("-")
+                self.mapping[source] = {
+                    target: []
+                }
+
+            elif line[0].isdigit():
+                output, start, width = [int(number) for number in line.split(" ")]
+                self.mapping[source][target].append([output, start, width])
+
+        part1 = self.seed_location(seeds)
+
+        part2 = 0
+        for i in range(0, len(seeds), 2):
+            start, length = seeds[i:i + 2]
+            lowest = self.seed_location(range(start, start + length))
+            if (not part2) or (lowest < part2):
+                part2 = lowest
+
+        end = time.perf_counter()
+
+        print(f"Lowest location part 1: {part1}")
+        print(f"Lowest location part 2: {part2}")
+        print(f"Total duration: {end - begin}s")
 
 if __name__ == "__main__":
-    planting()
+    Planting()
