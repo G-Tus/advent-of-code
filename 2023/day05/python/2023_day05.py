787b6cc202569be0c3c4b6b30695004fd83d5273
Gert-Jan van der Ham <github@g-tus.nl>
2023-12-13 19:32:54 +0100

    Refactored 2023 day 5 part 2 in Python to use algorithm

--- 2023/day05/python/2023_day05.py
+++ 2023/day05/python/2023_day05.py
@@ -6,31 +6,41 @@ class Planting:
         self.mapping = {}
         self.planting()
 
-    def seed_location(self, seeds):
+    def seed_location(self, seed):
+        source = "seed"
+        widths = []
+        while True:
+            target = list(self.mapping[source].keys())[0]
+
+            for output, start, width in self.mapping[source][target]:
+                stop = start + width
+                if (seed >= start) and (seed < stop):
+                    offset = seed - start
+                    widths.append(stop - seed)
+                    seed = output + offset    
+                    break
+
+            if target not in self.mapping:
+                break          
+
+            source = target
+            
+        return seed, widths
+    
+    def range_finding(self, start, stop):
         lowest = 0
+        while True:
+            location, widths = self.seed_location(start)
 
-        for seed in seeds:
-            source = "seed"
+            if (not lowest) or (location < lowest):
+                lowest = location
 
-            while True:
-                target = list(self.mapping[source].keys())[0]
+            start = start + min(widths)
 
-                for output, start, width in self.mapping[source][target]:
+            if start > stop:
+                break
 
-                    if (seed >= start) and (seed < (start + width)):
-                        offset = seed - start
-                        seed = output + offset    
-                        break
-
-                if target not in self.mapping:
-                    if (not lowest) or (seed < lowest):
-                        lowest = seed
-
-                    break          
-
-                source = target
-
-        return lowest
+        return lowest           
 
     def planting(self):
 
@@ -57,12 +67,17 @@ class Planting:
                 output, start, width = [int(number) for number in line.split(" ")]
                 self.mapping[source][target].append([output, start, width])
 
-        part1 = self.seed_location(seeds)
+        part1 = 0
+        for seed in seeds:
+            location, _ = self.seed_location(seed)
+            if (not part1) or (location < part1):
+                part1 = location
 
         part2 = 0
         for i in range(0, len(seeds), 2):
             start, length = seeds[i:i + 2]
-            lowest = self.seed_location(range(start, start + length))
+            stop = start + length
+            lowest = self.range_finding(start, stop)
             if (not part2) or (lowest < part2):
                 part2 = lowest
 
