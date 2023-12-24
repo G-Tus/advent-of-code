528df1b8abd41f6cc0e0133318b7c78db34ed277
Gert-Jan van der Ham <github@g-tus.nl>
2023-12-24 09:49:36 +0100

    Fixed 2023 day 8 part 2 in Python

--- 2023/day08/python/2023_day08.py
+++ 2023/day08/python/2023_day08.py
@@ -1,4 +1,5 @@
 import re
+import math
 
 def wasteland():
     
@@ -30,6 +31,44 @@ def wasteland():
             break
 
     print(f"Steps required to reach ZZZ: {step}")
+
+    nodes = []
+
+    for node in network:
+        if node[-1] == "A":
+            nodes.append(node)
+
+    step = 0
+
+    i = 0
+
+    counts = [[0, 0] for _ in range(6)]
+
+    while True:
+        instruction = 0 if instructions[i] == "L" else 1
+
+        for index, node in enumerate(nodes):
+            nodes[index] = network[node][instruction]
+
+        step += 1
+        i += 1
+
+        if i >= length:
+            i = 0
+
+        z = [(node[-1] == 'Z') for node in nodes]
+        if any(z):
+
+            for index, state in enumerate(z):
+                if state:
+                    counts[index][1] = step - counts[index][0]
+                    counts[index][0] = step
+            
+        if all([(count[1] != 0) for count in counts]):
+            break
+    
+    lcm = math.lcm(*[count[1] for count in counts])
+    print(f"Steps required to reach nodes all ending in Z: {lcm}")
 
 if __name__ == "__main__":
     wasteland()
