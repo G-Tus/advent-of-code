023d1158af0385bacbe626f8f9c97d54127d6552
Gert-Jan van der Ham <github@g-tus.nl>
2023-12-24 13:10:49 +0100

    Fixed 2023 day 09 part 2 in Python

--- 2023/day09/python/2023_day09.py
+++ 2023/day09/python/2023_day09.py
@@ -3,16 +3,17 @@ def oasis():
     with open("../input.txt", "r") as file:
         data = file.read().splitlines()
 
-    total = 0
+    part1 = 0
+    part2 = 0
 
     for line in data:
 
         values = [int(value) for value in line.split(" ")]
         ends = []
+        begins = []
 
         while True:
             differences = []
-            zero = True
             for index, value in enumerate(values[1:]):
                 differences.append(value - values[index])
 
@@ -20,15 +21,23 @@ def oasis():
                 break
 
             ends.append(value)
+            begins.append(values[0])
             values = differences
 
         value = 0
         for end in ends[::-1]:
             value += end
 
-        total += value
+        part1 += value
 
-    print(f"Total of extrapolated values: {total}")
+        value = 0
+        for begin in begins[::-1]:
+            value = begin - value
+
+        part2 += value
+
+    print(f"Total of extrapolated values endings: {part1}")
+    print(f"Total of extrapolated values beginnings: {part2}")
     
 if __name__ == "__main__":
     oasis()
