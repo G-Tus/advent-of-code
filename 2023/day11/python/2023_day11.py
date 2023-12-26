39dc58f5da64bf4b1e78a3e0ff39e09d6fded688
Gert-Jan van der Ham <github@g-tus.nl>
2023-12-26 13:20:53 +0100

    Fixed 2023 day 11 part 2 in Python

--- 2023/day11/python/2023_day11.py
+++ 2023/day11/python/2023_day11.py
@@ -6,26 +6,44 @@ def galaxies():
 
     data = np.array([[galaxy == "#" for galaxy in line] for line in data])
 
-    for index in range(len(data) - 1, -1,  -1):
-        row = data[index, :]
-        if not any(row):
-            data = np.insert(data, index, row, axis=0)
+    rows = []
+    columns = []
+    for index in range(len(data)):
+        if not any(data[index, :]):
+            rows.append(index)
 
-        column = data[:, index]
-        if not any(column):
-            data = np.insert(data, index, column, axis=1)
+        if not any(data[:, index]):
+            columns.append(index)
 
     locations = np.where(data)
     locations = list(zip(locations[0], locations[1]))
 
-    total = 0
+    total1 = 0
+    total2 = 0
     while locations:
         galaxy = locations.pop(0)
 
         for target in locations:
-            total += (abs(galaxy[0] - target[0]) + abs(galaxy[1] - target[1]))
+            distance = (abs(galaxy[0] - target[0]) + abs(galaxy[1] - target[1]))
 
-    print(f"Total distances between galaxies: {total}")
+            total1 += distance
+            total2 += distance
+
+            row_range = range(min(galaxy[0], target[0]), max(galaxy[0], target[0]))
+            for row in rows:
+                if row in row_range:
+                    total1 += 1
+                    total2 += 999999
+
+            column_range = range(min(galaxy[1], target[1]), max(galaxy[1], target[1]))
+            for column in columns:
+                if column in column_range:
+                    total1 += 1
+                    total2 += 999999
+
+    print(f"Total distances between galaxies part 1: {total1}")
+    print(f"Total distances between galaxies part 2: {total2}")
+
 
 
 if __name__ == "__main__":
