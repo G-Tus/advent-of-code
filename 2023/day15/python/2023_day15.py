94081d0f4779d9e8c92be0ea3eda85a52b31c19d
Gert-Jan van der Ham <github@g-tus.nl>
2024-01-03 22:30:55 +0100

    Fixed 2023 day 15 part 2 in Python

--- 2023/day15/python/2023_day15.py
+++ 2023/day15/python/2023_day15.py
@@ -1,22 +1,42 @@
-def hash_algorithm():
+def hash_algorithm(sequence):
+    value = 0
+
+    for character in sequence:
+        value += ord(character)
+        value *= 17
+        value %= 256
+
+    return value
+
+def lens_library():
 
     with open("../input.txt", "r") as file:
         data = file.read().split(",")
     
-    
     total = 0
+    boxes = [{} for _ in range(256)]
 
     for sequence in data:
-        value = 0
+        total += hash_algorithm(sequence)
 
-        for character in sequence:
-            value += ord(character)
-            value *= 17
-            value %= 256
+        if "=" in sequence:
+            label, focal = sequence.split("=")
+            value = hash_algorithm(label)
+            boxes[value][label] = int(focal)
 
-        total += value
+        if "-" in sequence:
+            label = sequence[:-1]
+            value = hash_algorithm(label)
+            boxes[value].pop(label, None)
+
+    power = 0
+
+    for i, box in enumerate(boxes, 1):
+        for j, focal in enumerate(box.values(), 1):
+            power += i * j * focal
 
     print(f"Sum of hash results: {total}")
+    print(f"Focusing power of resulting lens configuration: {power}")
 
 if __name__ == "__main__":
-    hash_algorithm()
+    lens_library()
