a48d238f9ce8b8715972816508cd51a9137dfff1
Gert-Jan van der Ham <github@g-tus.nl>
2024-10-25 05:36:57 +0200

    Fixed 2016 day 2 part 2 in Python

--- 2016/day02/python/2016_day02.py
+++ 2016/day02/python/2016_day02.py
@@ -1,15 +1,21 @@
 KEYPAD = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
+    ["1", "2", "3"],
+    ["4", "5", "6"],
+    ["7", "8", "9"]
 ]
 
-def bathroom_code():
-    with open("../input.txt", "r") as file:
-        data = file.read().splitlines()
+KEYPAD2 = [
+    ["0", "0", "1", "0", "0"],
+    ["0", "2", "3", "4", "0"],
+    ["5", "6", "7", "8", "9"],
+    ["0", "A", "B", "C", "0"],
+    ["0", "0", "D", "0", "0"]
+]
 
-    x = 1
-    y = 1
+def bathroom_code(data: list[str], x: int, y: int, keypad: list[list[str]]):
+    limit_x = len(keypad[0]) - 1
+    limit_y = len(keypad) - 1
+
     code = ""
 
     for line in data:
@@ -18,28 +24,31 @@ def bathroom_code():
             match move:
                 case "U":
                     y -= 1
-                    if y < 0:
-                        y = 0
+                    if y < 0 or keypad[y][x] == "0":
+                        y += 1
 
                 case "D":
                     y += 1
-                    if y > 2:
-                        y = 2
+                    if y > limit_y or keypad[y][x] == "0":
+                        y -= 1
 
                 case "L":
                     x -= 1
-                    if x < 0:
-                        x = 0
-                
+                    if x < 0 or keypad[y][x] == "0":
+                        x += 1
+
                 case "R":
                     x += 1
-                    if x > 2:
-                        x = 2
+                    if x > limit_x or keypad[y][x] == "0":
+                        x -= 1
 
-        code += str(KEYPAD[y][x])
+        code += keypad[y][x]
 
     print(f"Bathroom code: {code}")
 
 if __name__ == "__main__":
-    bathroom_code()
+    with open("../input.txt", "r") as file:
+        data = file.read().splitlines()
 
+    bathroom_code(data, 1, 1, KEYPAD)
+    bathroom_code(data, 0, 2, KEYPAD2)
