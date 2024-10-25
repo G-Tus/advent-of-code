56b9a216cf6935bebb2b06c450c8d1ff7e4fbf03
Gert-Jan van der Ham <github@g-tus.nl>
2024-10-25 23:41:09 +0200

    Fixed 2020 day 2 part 2 in Python

--- 2020/day02/python/2020_day02.py
+++ 2020/day02/python/2020_day02.py
@@ -3,11 +3,12 @@ def passwords():
         data = file.read().splitlines()
 
     correct = 0
+    correct2 = 0
 
     for row in data:
         policy, password = row.split(": ")
-        min_max, letter = policy.split(" ")
-        minimum, maximum = [int(value) for value in min_max.split("-")]
+        numbers, letter = policy.split(" ")
+        first, second = [int(value) for value in numbers.split("-")]
 
         count = 0
 
@@ -15,10 +16,14 @@ def passwords():
             if character == letter:
                 count += 1
 
-        if count in range(minimum, maximum + 1):
+        if count in range(first, second + 1):
             correct += 1
 
-    print(f"Number of correct passwords: {correct}")
+        if (password[first - 1] == letter) ^ (password[second - 1] == letter):
+            correct2 += 1
+ 
+    print(f"Number of correct passwords part 1: {correct}")
+    print(f"Number of correct passwords part 2: {correct2}")
 
 if __name__ == "__main__":
     passwords()
