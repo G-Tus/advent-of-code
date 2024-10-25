f63fc4c2a6e8b537f7b7e0da1cbd72964d7c671a
Gert-Jan van der Ham <github@g-tus.nl>
2024-10-25 19:25:54 +0200

    Fixed 2017 day 2 part 2 in Python

--- 2017/day02/python/2017_day02.py
+++ 2017/day02/python/2017_day02.py
@@ -1,13 +1,25 @@
+from copy import deepcopy
+
 def checksum():
     with open("../input.txt", "r") as file:
         data = [[int(number) for number in row.split("\t")] for row in file.read().splitlines()]
 
     checksum = 0
+    even_checksum = 0
 
     for row in data:
         checksum += (max(row) - min(row))
 
+        for index, number in enumerate(row):
+            divisors = deepcopy(row)
+            divisors.pop(index)
+
+            for divisor in divisors:
+                if number % divisor == 0:
+                    even_checksum += number // divisor
+
     print(f"Spreadsheet checksum: {checksum}")
+    print(f"Spreadsheet even checksum: {even_checksum}")
 
 if __name__ == "__main__":
     checksum()
