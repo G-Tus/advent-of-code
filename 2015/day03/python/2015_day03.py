adf3554279c9c0a4a2f39aa5283ebd1f68ef5613
Gert-Jan van der Ham <github@g-tus.nl>
2024-10-26 09:38:35 +0200

    Fixed 2015 day 3 part 2 in Python

--- 2015/day03/python/2015_day03.py
+++ 2015/day03/python/2015_day03.py
@@ -1,30 +1,54 @@
+from copy import deepcopy
+
+START = (0, 0)
+
 def presents():
     with open("../input.txt", "r") as file:
         data = file.read()
 
-    x = 0
-    y = 0
-
-    houses = [(x, y)]
+    house = deepcopy(START)
+    houses = [house]
 
     for step in data:
-        match step:
-            case "<":
-                x -= 1
-            
-            case "^":
-                y += 1
-
-            case ">":
-                x += 1
-
-            case "v":
-                y -= 1
-
-        if (house := (x, y)) not in houses:
+        house = move(step, house)
+        
+        if house not in houses:
             houses.append(house)
 
     print(f"Houses with presents: {len(houses)}")
+
+    santa = deepcopy(START)
+    robo = deepcopy(START)
+    houses = [santa]
+
+    for i in range(0, len(data), 2):
+        santa = move(data[i], santa)
+        robo = move(data[i + 1], robo)
+
+        if santa not in houses:
+            houses.append(santa)
+
+        if robo not in houses:
+            houses.append(robo)
+
+    print(f"Houses with presents teamwork: {len(houses)}")
+
+def move(step, position):
+    x, y = position
+    match step:
+        case "<":
+            x -= 1
+        
+        case "^":
+            y += 1
+
+        case ">":
+            x += 1
+
+        case "v":
+            y -= 1
+
+    return (x, y)
 
 if __name__ == "__main__":
     presents()
