fa503a8b50c4f295efff3b528fe2e1dc76293dfe
Gert-Jan van der Ham <github@g-tus.nl>
2024-10-27 11:33:09 +0100

    Fixed 2015 day 6 part 2 in Python

--- 2015/day06/python/2015_day06.py
+++ 2015/day06/python/2015_day06.py
@@ -3,6 +3,7 @@ def lights():
         instructions = file.read().splitlines()
 
     grid = [[False for _ in range(1000)] for _ in range(1000)]
+    brightness = [[0 for _ in range(1000)] for _ in range(1000)]
 
     for instruction in instructions:
         parts = instruction.split(" ")
@@ -10,21 +11,28 @@ def lights():
         start_x, start_y = [int(number) for number in parts[-3].split(",")]
         stop_x, stop_y = [int(number) + 1 for number in parts[-1].split(",")]
 
+        size = (stop_x - start_x) * (stop_y - start_y)
+
         match parts[1]:
             case "on":
                 switch = lambda x: True
+                control = lambda x: x + 1
 
             case "off":
                 switch = lambda x: False
+                control = lambda x: 0 if x - 1 < 0 else x - 1
 
             case _:
                 switch = lambda x: not x
+                control = lambda x: x + 2
 
         for x in range(start_x, stop_x):
             for y in range(start_y, stop_y):
                 grid[y][x] = switch(grid[y][x])
+                brightness[y][x] = control(brightness[y][x])
 
     print(f"Lights on: {sum(sum(row) for row in grid)}")
+    print(f"Total brightness: {sum(sum(row) for row in brightness)}")
 
 if __name__ == "__main__":
     lights()
