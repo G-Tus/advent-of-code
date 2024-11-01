faafb7f22f23166dd4e1e8c9cafe51c99db3395e
Gert-Jan van der Ham <github@g-tus.nl>
2024-11-01 22:41:05 +0100

    Fixed 2015 day 1 part 2 in Rust

--- 2015/day01/rust/src/main.rs
+++ 2015/day01/rust/src/main.rs
@@ -6,17 +6,25 @@ fn main() -> io::Result<()> {
     let reader = BufReader::new(file);
 
     let mut floor = 0;
+    let mut position = 0;
+    let mut first: Option<i32> = None;
 
     for char in reader.bytes() {
+        position += 1;
         let step = char? as char;
         match step {
             '(' => floor += 1,
             ')' => floor -= 1,
             _ => continue,          
         }
+
+        if floor == -1 && first.is_none() {
+            first = Some(position);
+        }
     }
 
     println!("Santa ends up at floor: {}", floor);
+    println!("Santa enter basements at position: {:?}", first);
 
     Ok(())
 }
