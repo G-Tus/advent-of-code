917abf0a1cf430bf040e9e54e1fb49e6cf4b50b2
Gert-Jan van der Ham <github@g-tus.nl>
2024-11-02 15:17:27 +0100

    Fixed 2015 day 1 part 2 in TypeScript

--- 2015/day01/typescript/2015_day01.ts
+++ 2015/day01/typescript/2015_day01.ts
@@ -3,15 +3,21 @@ import { readFileSync } from 'fs';
 const steps: string = readFileSync('../input.txt', 'utf8')
 
 let floor: number = 0;
+let position: number = 0;
+let first: number = 0;
 
 for (let step of steps) {
+    position++;
     if (step == "(") {
         floor += 1;
     } else if (step == ")") {
         floor -= 1;
-    } else {
-        continue
+    }
+
+    if (floor == -1 && first == 0) {
+        first = position;
     }
 }
 
 console.log(`Santa ends up at floor: ${floor}`);
+console.log(`Santa enters basement at step: ${first}`);
