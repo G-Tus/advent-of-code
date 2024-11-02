597293bb9d99a278830a67b670494db66d0f4cd1
Gert-Jan van der Ham <github@g-tus.nl>
2024-11-02 14:35:59 +0100

    Optimized and fixes 2015 day 1 part 2 in COBOL

--- 2015/day01/cobol/2015_day01.cbl
+++ 2015/day01/cobol/2015_day01.cbl
@@ -1,6 +1,6 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID. AoC_2015_day01.
-             
+
        ENVIRONMENT DIVISION.
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
@@ -8,44 +8,49 @@
            ASSIGN TO "../input.txt"
            ORGANIZATION IS SEQUENTIAL
            FILE STATUS IS FILE-STATUS.
-       
+
        DATA DIVISION.
        FILE SECTION.
        FD  INPUT-FILE.
        01  INPUT-CHAR  PIC X.
-       
+
        WORKING-STORAGE SECTION.
        01 FILE-STATUS PIC X(2).
            88 FILE-OK  VALUE "00".
            88 FILE-EOF VALUE "10".
-       01 END-OF-FILE-FLAG     PIC X VALUE "N".
-           88 END-OF-FILE VALUE "Y".
        01 FLOOR PIC S9(8) VALUE 0.
-       
+           88 UNDERGROUND VALUES -99999999 THRU -1.
+       01 STEPS PIC 9(8) VALUE 0.
+       01 LOCATION PIC 9(8) VALUE 0.
+           88 FIRST-TIME VALUE 0.
+
        PROCEDURE DIVISION.
        MAIN-PARA.
            PERFORM OPEN-FILE
-           PERFORM READ-STEPS UNTIL END-OF-FILE
+           PERFORM READ-STEPS UNTIL FILE-EOF
            DISPLAY "Santa ends on floor: " FLOOR
+           DISPLAY "Santa goes to the basement at step: " LOCATION
            PERFORM CLOSE-FILE
            STOP RUN.
-       
+
        OPEN-FILE.
            OPEN INPUT INPUT-FILE
            IF NOT FILE-OK
                DISPLAY "Error opening file. Status: " FILE-STATUS
                STOP RUN
            END-IF.
-       
+
        READ-STEPS.
            READ INPUT-FILE
                AT END
-                   SET END-OF-FILE TO TRUE
+                   SET FILE-EOF TO TRUE
                NOT AT END
                    PERFORM PROCESS-STEP
            END-READ.
-       
+
        PROCESS-STEP.
+           ADD 1 TO STEPS.
+
            EVALUATE INPUT-CHAR
                WHEN "("
                    PERFORM FLOOR-UP
@@ -53,11 +58,15 @@
                    PERFORM FLOOR-DOWN
            END-EVALUATE.
 
+           IF UNDERGROUND AND FIRST-TIME
+               MOVE STEPS TO LOCATION
+           END-IF.
+
        FLOOR-UP.
            ADD 1 TO FLOOR.
-           
+
        FLOOR-DOWN.
            SUBTRACT 1 FROM FLOOR.
-       
+
        CLOSE-FILE.
            CLOSE INPUT-FILE.
