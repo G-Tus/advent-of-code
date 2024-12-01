a522c6a7b7785a7f56be432dbb05a18670e9e4a8
Gert-Jan van der Ham <github@g-tus.nl>
2024-12-01 09:18:21 +0100

    Fixed 2024 day 1 part 2 in Python

--- 2024/day01/python/2024_day01.py
+++ 2024/day01/python/2024_day01.py
@@ -17,5 +17,21 @@ def locations():
 
     print(f"Total distance between lists: {sum(difference)}")
 
+    similarity = 0
+
+    for left_number in left:
+        count = 0
+
+        for right_number in right:
+            if left_number < right_number:
+                break
+
+            elif left_number == right_number:
+                count += 1
+
+        similarity += left_number * count
+
+    print(f"Similarity score: {similarity}")
+
 if __name__ == "__main__":
     locations()
