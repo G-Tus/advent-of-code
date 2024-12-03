793cfbfbf1b576884c2a3b72ba7ffcbcd5e5ef3f
Gert-Jan van der Ham <github@g-tus.nl>
2024-12-03 07:51:58 +0100

    Fixed 2024 day 3 part 2 in Python

--- 2024/day03/python/2024_day03.py
+++ 2024/day03/python/2024_day03.py
@@ -1,18 +1,35 @@
-from re import findall
+from re import finditer
 
 def corruption():
     with open("../input.txt", "r") as file:
         data = file.read()
         
-    matches = findall(r"mul\((\d+),(\d+)\)", data)
+    multiplications = {match.start(): match.groups() for match in finditer(r"mul\((\d+),(\d+)\)", data)}
+    conditionals = {match.start(): match.group() for match in finditer(r"don?'?t?\(\)", data)}
+    matches = dict(sorted((multiplications | conditionals).items()))
 
+    active = True
     total = 0
+    conditional_total = 0
 
-    for match in matches:
-        left, right = match
-        total += int(left) * int(right)
+    for item in matches.values():
+        match item:
+            case "do()":
+                active = True
+
+            case "don't()":
+                active = False
+
+            case _:
+                left, right = [int(number) for number in item]
+                multiplication = left * right
+                total += multiplication
+
+                if active:
+                    conditional_total += multiplication
 
     print(f"Total of multiplications: {total}")
+    print(f"Total of conditional multiplications: {conditional_total}")
 
 if __name__ == "__main__":
     corruption()
