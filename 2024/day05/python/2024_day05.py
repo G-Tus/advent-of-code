bcc5e2207e97cfa5699de36436e2c41d6f06633d
Gert-Jan van der Ham <github@g-tus.nl>
2024-12-07 10:58:00 +0100

    Optimized 2024 day 5 Python

--- 2024/day05/python/2024_day05.py
+++ 2024/day05/python/2024_day05.py
@@ -1,3 +1,5 @@
+from functools import cmp_to_key
+
 def updates():
     with open("../input.txt", "r") as file:
         data = file.read().splitlines()
@@ -14,65 +16,28 @@ def updates():
             update = line.split(",")
             updates.append(update)
 
+    lookup = {}
+
+    for left, right in rules:
+        if left not in lookup:
+            lookup[left] = [right]
+
+        else:
+            lookup[left].append(right)
+
     total = 0
     total_corrected = 0
+    compare = lambda left, right: 1 if left in lookup.get(right, []) else -1
 
     for update in updates:
-        correct = True
-        lookup = {}
+        corrected = sorted(update, key=cmp_to_key(compare))
+        index = len(update) // 2
 
-        for left, right in rules:
-            if left not in update or right not in update:
-                continue
-
-            if left not in lookup:
-                lookup[left] = [right]
-
-            else:
-                lookup[left].append(right)
-
-            for index, page in enumerate(update):
-                if left == page:
-                    if right in update[:index + 1]:
-                        correct = False
-
-        if correct:
-            index = len(update) // 2
+        if corrected == update:
             total += int(update[index])
 
         else:
-            for number, befores in lookup.items():
-                for before in befores:
-                    if before in lookup:
-                        for extra_before in lookup[before]:
-                            if extra_before not in befores:
-                                befores.append(extra_before)
-
-            lookup = list(lookup.items())
-
-            for number, befores in lookup:
-                i = None
-
-                for index in range(len(update) - 1, -1, -1):
-                    page = update[index]
-                    if page == number:
-                        i = index
-
-                    if page in befores:
-                        befores.remove(page)
-
-                    if not befores:
-                        j = index
-                        break
-
-                if i == None:
-                    continue
-
-                switch = update.pop(i)
-                update.insert(j, switch)
-
-            index = len(update) // 2
-            total_corrected += int(update[index])
+            total_corrected += int(corrected[index])
 
     print(f"Total of middle pages: {total}")
     print(f"Total of corrected middle pages: {total_corrected}")
