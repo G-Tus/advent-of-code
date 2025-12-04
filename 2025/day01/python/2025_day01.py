cdaac6a16d3c40e069ce2d138f7f4b09eb66a20b
Gert-Jan van der Ham <github@g-tus.nl>
2025-12-04 21:10:39 +0100

    Fixed 2025 day 1 part 2 in Python

--- 2025/day01/python/2025_day01.py
+++ 2025/day01/python/2025_day01.py
@@ -6,10 +6,17 @@ def safe():
     total = 0
 
     for action in input:
-        rotation = (rotation + (-1 if action[0] == "L" else 1) * int(action[1:])) % 100
+        for _ in range(int(action[1:])):
+            rotation += -1 if action[0] == "L" else 1
 
-        if rotation == 0:
-            total += 1
+            if rotation > 99:
+                rotation = 0
+
+            elif rotation < 0:
+                rotation = 99
+
+            if rotation == 0:
+                total += 1
 
     print(f"Total times at rotation 0: {total}")
 
